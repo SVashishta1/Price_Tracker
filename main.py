e8266b4d360247542665bd0ac3debd770c3e0ab8
SVashishta1 <user969gp@gmail.com>
2023-12-04 15:49:52 -0500

    SQL code change

--- main.py
+++ main.py
@@ -49,12 +49,29 @@ def fetch_current_price(product_id):
     try:
         with mysql.connector.connect(**db_config) as connection:
             cursor = connection.cursor()
-            cursor.execute("SELECT MIN(price) AS min_price FROM products WHERE p_id = %s", (product_id,))
-            min_price = cursor.fetchone()[0]
-            return min_price
+            # Retrieve the lowest price from the most recent data
+            cursor.execute("select MIN(PRICE) from (SELECT PRICE FROM products WHERE p_id = %s ORDER BY scrape_date DESC LIMIT 3) as x;", (product_id,))
+            min_price = cursor.fetchone()
+            
+            if min_price:
+                return min_price[0]
+            else:
+                # Handle the case where no data is available
+                return None
     except mysql.connector.Error as err:
         print(f"Error: {err}")
         return None
+
+# def fetch_current_price(product_id):
+#     try:
+#         with mysql.connector.connect(**db_config) as connection:
+#             cursor = connection.cursor()
+#             cursor.execute("SELECT MIN(price) AS min_price FROM products WHERE p_id = %s", (product_id,))
+#             min_price = cursor.fetchone()[0]
+#             return min_price
+#     except mysql.connector.Error as err:
+#         print(f"Error: {err}")
+#         return None
 
 
 # Function to generate the bar graph
@@ -191,79 +208,6 @@ def airpods():
 
 
 
-
-
-# @app.route("/airpods.html")
-# def airpods():
-#     product_id = 1  # Replace with the actual product ID for AirPods
-#     product_name = "AirPods"  # Replace with the actual product name
-#     current_price = fetch_current_price(product_id)
-#     price_data = fetch_price_data(product_id)
-
-#     # Generate line graph data
-#     line_graph = generate_line_graph(price_data)
-#     line_graph_html = line_graph.to_html(full_html=False) if line_graph else None
-
-#     # Generate bar graph data
-#     bar_graph = generate_bar_graph(price_data)
-#     bar_graph_html = bar_graph.to_html(full_html=False) if bar_graph else None
-
-    
-
-
-#     # Convert 'scrape_date' values to datetime objects
-#     date_objects = [datetime.strptime(str(entry['scrape_date']), '%Y-%m-%d') for entry in price_data]
-
-# # Generate future timestamps
-#     future_timestamps = [max(date_objects) + pd.DateOffset(days=i) for i in range(1, 6)]
-#     # Train linear regression model
-#     model = train_linear_regression_model(price_data)
-
-#     # Predict future prices
-#     future_predictions = predict_prices(model, future_timestamps)
-
-#     return render_template(
-#     "airpods.html",
-#     product_name=product_name,
-#     current_price=current_price,
-#     line_graph=line_graph_html,
-#     bar_graph=bar_graph_html,
-#     future_timestamps=future_timestamps,
-#     predicted_prices=future_predictions  # Updated variable name
-# )
-
-# @app.route("/airpods.html")
-# def airpods():
-#     product_id = 1  # Replace with the actual product ID for AirPods
-#     product_name = "AirPods"  # Replace with the actual product name
-#     current_price = fetch_current_price(product_id)
-#     price_data = fetch_price_data(product_id)
-#     line_graph = generate_line_graph(price_data)
-#     line_graph_html = line_graph.to_html(full_html=False) if line_graph else None
-
-#     # Generate bar graph data
-#     bar_graph = generate_bar_graph(price_data)
-#     bar_graph_html = bar_graph.to_html(full_html=False) if bar_graph else None
-
-#     return render_template("airpods.html", product_name=product_name, current_price=current_price, line_graph=line_graph_html, bar_graph=bar_graph_html)
-
-
-
-# @app.route("/doorbell.html")
-# def doorbell():
-#     product_id = 2  # Replace with the actual product ID for the doorbell
-#     product_name = "Security Camera"  # Replace with the actual product name
-#     current_price = fetch_current_price(product_id)
-#     price_data = fetch_price_data(product_id)
-#     line_graph = generate_line_graph(price_data)
-#     line_graph_html = line_graph.to_html(full_html=False) if line_graph else None
-
-#     # Generate bar graph data
-#     bar_graph = generate_bar_graph(price_data)
-#     bar_graph_html = bar_graph.to_html(full_html=False) if bar_graph else None
-
-#     return render_template("doorbell.html", product_name=product_name, current_price=current_price, line_graph=line_graph_html, bar_graph=bar_graph_html)
-
 @app.route("/doorbell.html")
 def doorbell():
     product_id = 2  # Replace with the actual product ID for the doorbell
@@ -311,20 +255,6 @@ def doorbell():
 
 
 
-# @app.route("/electric_cooker.html")
-# def electric_cooker():
-#     product_id = 3  # Replace with the actual product ID for the electric cooker
-#     product_name = "Electric Cooker"  # Replace with the actual product name
-#     current_price = fetch_current_price(product_id)
-#     price_data = fetch_price_data(product_id)
-#     line_graph = generate_line_graph(price_data)
-#     line_graph_html = line_graph.to_html(full_html=False) if line_graph else None
-
-#     # Generate bar graph data
-#     bar_graph = generate_bar_graph(price_data)
-#     bar_graph_html = bar_graph.to_html(full_html=False) if bar_graph else None
-
-#     return render_template("electric_cooker.html", product_name=product_name, current_price=current_price, line_graph=line_graph_html, bar_graph=bar_graph_html)
 
 @app.route("/electric_cooker.html")
 def electric_cooker():
@@ -373,20 +303,7 @@ def electric_cooker():
 
 
 
-# @app.route("/meta_quest_3.html")
-# def meta_quest_3():
-#     product_id = 4  # Replace with the actual product ID for the Meta Quest 3
-#     product_name = "Meta Quest 3"  # Replace with the actual product name
-#     current_price = fetch_current_price(product_id)
-#     price_data = fetch_price_data(product_id)
-#     line_graph = generate_line_graph(price_data)
-#     line_graph_html = line_graph.to_html(full_html=False) if line_graph else None
 
-#     # Generate bar graph data
-#     bar_graph = generate_bar_graph(price_data)
-#     bar_graph_html = bar_graph.to_html(full_html=False) if bar_graph else None
-
-#     return render_template("meta_quest_3.html", product_name=product_name, current_price=current_price, line_graph=line_graph_html, bar_graph=bar_graph_html)
 
 @app.route("/meta_quest_3.html")
 def meta_quest_3():
@@ -440,20 +357,6 @@ def meta_quest_3():
 
 
 
-# @app.route("/vaccum.html")
-# def vaccum():
-#     product_id = 5  # Replace with the actual product ID for the vacuum
-#     product_name = "Vacuum"  # Replace with the actual product name
-#     current_price = fetch_current_price(product_id)
-#     price_data = fetch_price_data(product_id)
-#     line_graph = generate_line_graph(price_data)
-#     line_graph_html = line_graph.to_html(full_html=False) if line_graph else None
-
-#     # Generate bar graph data
-#     bar_graph = generate_bar_graph(price_data)
-#     bar_graph_html = bar_graph.to_html(full_html=False) if bar_graph else None
-
-#     return render_template("vaccum.html", product_name=product_name, current_price=current_price, line_graph=line_graph_html, bar_graph=bar_graph_html)
 
 @app.route("/vaccum.html")
 def vaccum():
@@ -505,43 +408,9 @@ def vaccum():
 
 
 
-# # ... (existing code)
 
-# # Route to product details page with line graph and prediction
-# @app.route("/product/<product_id>")
-# def product_details_with_prediction(product_id):
-#     # Fetch product details from the database
-#     cursor.execute("SELECT product_name FROM products WHERE p_id = %s", (product_id,))
-#     product_name = cursor.fetchone()[0]
 
-#     # Fetch current price
-#     current_price = fetch_current_price(product_id)
 
-#     # Fetch timestamp and price data from the database
-#     timestamp_price_data = fetch_timestamp_price_data(product_id)
-
-#     # Generate line graph
-#     line_graph = generate_line_graph(timestamp_price_data)
-#     line_graph_html = line_graph.to_html(full_html=False) if line_graph else None
-
-#     # Generate bar graph data
-#     bar_graph = generate_bar_graph(timestamp_price_data)
-#     bar_graph_html = bar_graph.to_html(full_html=False) if bar_graph else None
-
-#     # Predict future prices
-#     future_predictions = predict_prices(timestamp_price_data)
-
-#     # Determine whether to wait or buy based on predictions
-#     prediction_message = "Wait" if future_predictions is not None and future_predictions[-1] > current_price else "Buy"
-
-#     return render_template(
-#         "product_details_with_prediction.html",
-#         product_name=product_name,
-#         current_price=current_price,
-#         line_graph=line_graph_html,
-#         bar_graph=bar_graph_html,
-#         prediction_message=prediction_message,
-#     )
 
 
 
